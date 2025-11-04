ba98d08d7a674983ee22858e28360effbe3b33e9
Ojemmy <Ojemmy30@gmail.com>
2025-11-04 16:14:59 +0100

    hero section video adjusted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -90,7 +90,7 @@ const Hero = () => {
         </section>
 
         <div className="video absolute inset-0">
-            <video ref={videoRef} src="/public/videos/output.mp4" muted playsInline preload="auto"/>
+            <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload="auto"/>
         </div>
     </>
   )
